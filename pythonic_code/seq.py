1f66ad2701c9a1140748a6ff5ce78a812912da1b
Bhuwan Bhatt <bhattbhuwan13@outlook.com>
2021-12-10 14:57:42 +0545

    added mypy type hints

--- pythonic_code/seq.py
+++ pythonic_code/seq.py
@@ -1,23 +1,24 @@
 from collections.abc import Sequence
 
 class Items(Sequence):
-    def __init__(self, *values):
+
+    def __init__(self, *values) -> None:
         self._values = list(values) 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self._values)
-    def __getitem__(self, item):
+    def __getitem__(self, item) -> Items:
         return self._values.__getitem__(item)
 
-
 class ItemsNew(Sequence):
-    def __init__(self, *values):
+    def __init__(self, *values) -> None:
         self._values = list(values) 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self._values)
-    def __getitem__(self, item):
+    def __getitem__(self, item) -> ItemsNew:
         return self._values[item]
 
 a = Items(1,2,3,4)
 print(a[1], a[1:3])
-a = ItemsNew(1,2,3,4)
-print(a[1], a[1:3])
+b = ItemsNew(1,2,3,4)
+print(b[1], b[1:3])
+
