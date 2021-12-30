5a2bf2a228df9f723cf22df1480f7abdc18c834c
Bhuwan Bhatt <bhattbhuwan13@outlook.com>
2021-12-30 10:12:17 +0545

    adjded notes on static methods

--- oops/shipping/shipping.py
+++ oops/shipping/shipping.py
@@ -6,9 +6,16 @@ class ShippingContainer:
 
     next_serial = 1337
 
+    # Static methods are used to group code that are related to a class rather than a specific object
+
+    @staticmethod
+    def _generate_serial(): # No self argument required for the static method, we are dealing with class variable and not instance variable
+        result = ShippingContainer.next_serial
+        ShippingContainer.next_serial += 1
+        return result
+
     def __init__(self, owner_code, contents):
         self.owner_code = owner_code
         self.contents = contents
-        self.serial = ShippingContainer.next_serial
-        ShippingContainer.next_serial += 1
+        self.serial = ShippingContainer._generate_serial()
 
